d28655b0ffb9700f067237124d7a961990b22018
conao3 <conao3@gmail.com>
2025-12-23 00:14:22 +0900

    add flake

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: all
+all:
+
+.PHONY: fmt
+fmt:
+	nix fmt
---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          packages.default = pkgs.writeShellApplication {
+            name = "gitm";
+            runtimeInputs = with pkgs; [
+              git
+              python3
+            ];
+            text = builtins.readFile ./gitm;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.shfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              bash
+              python3
+            ] ++ [ self'.packages.default ];
+          };
+        };
+    };
+}
